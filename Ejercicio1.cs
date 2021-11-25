9150f777615072542a4b2853ad78fc18f1392f5e
usanchez <usanchez@bancoagricola.com.sv>
2021-11-25 11:08:30 -0600

    Ejercicio1

---
+++ Ejercicio1.cs
@@ -0,0 +1,18 @@
+/* Ejercicio 1 */
+
+var numero1 = 5;
+var numero2 = 8;
+if (numero1 > numero2)
+{
+Console.WriteLine("num 1 es mayor que numero 2"); ;
+}
+if (numero2 >= 0) {
+Console.WriteLine("num 2 es positivo");
+}
+if (numero1 > 0 && numero1 != 0) {
+Console.WriteLine("num 1 es negativo o distinto de cero");
+}
+numero1 = numero1 + 1;
+if (numero1 != numero2) {
+Console.WriteLine("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
